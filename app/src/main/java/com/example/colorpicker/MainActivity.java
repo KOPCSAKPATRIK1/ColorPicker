868441f5efe5c1d4e79141ef658b6ded45aa06bb
KOPCSAKPATRIK1 <kopcsakpatrik1@gmail.com>
2022-09-17 14:43:15 +0200

    layout cuccok hozza adva

--- app/src/main/java/com/example/colorpicker/MainActivity.java
+++ app/src/main/java/com/example/colorpicker/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.colorpicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.slider.Slider;
+
+public class MainActivity extends AppCompatActivity
+{
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+
+
 }
