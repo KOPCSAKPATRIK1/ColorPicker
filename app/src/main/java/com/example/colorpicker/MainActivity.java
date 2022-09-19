6c749c15dbe3805eec383b460f9c2d24d23d5b9d
KOPCSAKPATRIK1 <kopcsakpatrik1@gmail.com>
2022-09-19 11:56:11 +0200

    csere seekbarra

--- app/src/main/java/com/example/colorpicker/MainActivity.java
+++ app/src/main/java/com/example/colorpicker/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.colorpicker;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import java.lang.Object;
+import java.text.NumberFormat;
+import java.util.Currency;
+
+import com.google.android.material.slider.LabelFormatter;
 import com.google.android.material.slider.Slider;
+
+
 
 public class MainActivity extends AppCompatActivity
 {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,10 +27,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
     }
-
-
-
 
 }
